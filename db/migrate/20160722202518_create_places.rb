5ea46aba10315cb4e60036f1f64d065ad74f87b8
Ben Zuba <zubes33@gmail.com>
2016-07-22 20:48:27 +0000

    added 2 places, and adjusted index.html.erb to list name as header, address in i, and description as paragraph

--- db/migrate/20160722202518_create_places.rb
+++ db/migrate/20160722202518_create_places.rb
@@ -1,10 +1,10 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
-
+      
       t.string :name
       t.text :description
-      tstring :address
+      t.string :address
 
       t.timestamps
     end
