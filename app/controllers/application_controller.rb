8d16983c190f31595bbdc8ceccd8fa8baf12307f
Ben Zuba <zubes33@gmail.com>
2016-07-25 20:12:59 +0000

    error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+Place.paginate(:page => params[:page], :per_page => 2)
