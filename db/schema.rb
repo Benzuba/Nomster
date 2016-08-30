f26e3af0d7014710ef84ba65aba6891aeeb01e47
Ben Zuba <zubes33@gmail.com>
2016-08-30 23:13:09 +0000

    adjusted places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819223248) do
+ActiveRecord::Schema.define(version: 20160819223852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160819223248) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
+    t.string   "Picture"
   end
 
   create_table "places", force: true do |t|
